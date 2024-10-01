e65e11f35fae6f6e5b93ef9e20eddf75864c065e
koren85 <mailbobik@gmail.com>
2024-10-01 14:32:25 +0300

    новая версия, теперь выпадашки

--- app/controllers/task_status_changer_controller.rb
+++ app/controllers/task_status_changer_controller.rb
@@ -1,5 +1,8 @@
+# /path/to/redmine/plugins/task_status_changer/app/controllers/task_status_changer_controller.rb
+
 class TaskStatusChangerController < ApplicationController
-  before_action :find_issue
+  before_action :find_issue, only: [:change_status]
+
   before_action :authorize_change_status, only: [:change_status]
 
   def change_status
@@ -9,20 +12,29 @@ class TaskStatusChangerController < ApplicationController
 
     if rule
       assign_user_if_allowed(rule['assign_user_id'])
-      update_custom_field_if_present(rule['custom_field_id'])
+
+      custom_field_options = params[:custom_field_options] || []
+
+      # Всегда обновляем общий счётчик, даже если конкретные поля не выбраны
+      update_custom_field_if_present(rule['general_counter']) if rule['general_counter'].present?
+
+      custom_field_options.each do |field_key|
+        # Получаем ID кастомного поля из настроек
+        custom_field_id = rule[field_key]
+        update_custom_field_if_present(custom_field_id) if custom_field_id.present?
+      end
 
       if rule['new_status']
         @issue.init_journal(User.current)
         @issue.status_id = rule['new_status']
         @issue.save!
-        flash[:notice] = 'Статус, исполнитель задачи и кастомное поле обновлены успешно.'
+        render json: { success: true, message: 'Статус и кастомные поля обновлены успешно.' }
       else
-        flash[:error] = 'Нет правила для изменения статуса.'
+        render json: { success: false, message: 'Нет правила для изменения статуса.' }
       end
     else
-      flash[:error] = 'Не найдено подходящее правило.'
+      render json: { success: false, message: 'Не найдено подходящее правило.' }
     end
-    redirect_to issue_path(@issue)
   end
 
   private
@@ -31,13 +43,26 @@ class TaskStatusChangerController < ApplicationController
     @issue = Issue.find(params[:issue_id])
   end
 
+  # def authorize_change_status
+  #   settings = Setting.plugin_task_status_changer['status_transition_rules'] || {}
+  #   rule = settings.values.find { |r| r['current_status'] == @issue.status_id.to_s }
+  #   return head :forbidden unless rule && (user_allowed?(rule['check_user_id']) || group_allowed?(rule['check_user_id']))
+  #
+  #   true
+  # end
+
   def authorize_change_status
     settings = Setting.plugin_task_status_changer['status_transition_rules'] || {}
-    rule = settings.values.find { |r| r['current_status'] == @issue.status_id.to_s }
-    return flash[:error] = 'Не найдено подходящее правило.' unless rule && (user_allowed?(rule['check_user_id']) || group_allowed?(rule['check_user_id']))
+    rule = settings.values.find { |r| r['current_status'] == @issue.status_id.to_s } # Используем @issue
 
+    return flash[:error] = 'Не найдено подходящее правило.' unless User.current.admin? || rule && (user_allowed?(rule['check_user_id']) || group_allowed?(rule['check_user_id']))
+   
+    Rails.logger.info("Пользователь #{User.current.login} имеет права для изменения статуса задачи ##{@issue.id}.")
     true
   end
+
+
+
 
   def user_allowed?(user_id)
     User.current.id.to_s == user_id.to_s
@@ -64,12 +89,12 @@ class TaskStatusChangerController < ApplicationController
       if custom_value
         new_value = custom_value.value.to_i + 1
         custom_value.value = new_value
-        @issue.init_journal(User.current, "Счетчик возвратов обновлен с #{custom_value.value.to_i - 1} на #{custom_value.value.to_i}")
+        @issue.init_journal(User.current, "Обновлено кастомное поле #{custom_field.name} с #{custom_value.value.to_i - 1} на #{custom_value.value.to_i}")
         @issue.save_custom_field_values
       end
     else
+      Rails.logger.error("Кастомное поле недоступно в задаче: #{custom_field_id}")
       flash[:error] = 'Выбранное кастомное поле недоступно в этой задаче.'
     end
   end
-
 end
--- assets/javascripts/task_status_changer.js
+++ assets/javascripts/task_status_changer.js
@@ -1,11 +1,10 @@
 // /path/to/redmine/plugins/task_status_changer/assets/javascripts/task_status_changer.js
 document.addEventListener('DOMContentLoaded', function() {
     var editButton = document.querySelector('a.icon-edit');
-    if (editButton) {
-        var changeStatusButton = document.getElementById('change-status-button');
-        if (changeStatusButton) {
-            editButton.insertAdjacentElement('beforebegin', changeStatusButton);
-            changeStatusButton.style.display = 'inline-block';
-        }
+    var modalButton = document.getElementById('open-modal-button'); // Обратите внимание, что мы ищем элемент по новому id
+    if (editButton && modalButton) {
+        // Вставляем кнопку перед кнопкой "Редактировать"
+        editButton.insertAdjacentElement('beforebegin', modalButton);
+        modalButton.style.display = 'inline-block'; // Отображаем кнопку
     }
 });
